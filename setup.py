e38c92f498341127a8d427e90afa0a82299abbe5
Felipe Santana <fsantanar@gmail.com>
2023-06-15 16:08:46 -0400

    shifting packes back to find_packages() in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='cpu_rtd_test',
     version='1.0.0',
-    packages=['rtdtest'],
+    packages=find_packages(),
     install_requires=[
         'numpy==1.24.3',
         'PyYAML==6.0',

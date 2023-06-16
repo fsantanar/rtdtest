43b1cd33e51cd9d0db69fed9cf5f0a28cde3a9d4
Felipe Santana <fsantanar@gmail.com>
2023-06-16 13:30:14 -0400

    adding path based on file instead of curr folder

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, Path(os.path.abspath(__file__)).parent.parent.parent.parent.absolute())
+sys.path.insert(0, Path(os.path.abspath(__file__)).parent.parent.parent.absolute())
 
 extensions = [
     'sphinx.ext.autodoc',

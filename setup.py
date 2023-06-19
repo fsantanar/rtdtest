e6adda7a47dabae28fbf514e0a9d533ef7bdaf9f
Felipe Santana <fsantanar@gmail.com>
2023-06-19 12:42:10 -0400

    using src name but not package dir in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='rtdtest',
     version='1.0.0',
-    packages=find_packages(),
-    package_dir={'':'src'},
+    packages=find_packages('src'),
     install_requires=[
         'numpy==1.24.3',
         'PyYAML==6.0',

55915852a3c3ac0fe90d549a703accd547f1fc8b
Felipe Santana <fsantanar@gmail.com>
2023-06-19 12:35:34 -0400

    putting src name back in find_packages in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='rtdtest',
     version='1.0.0',
-    packages=find_packages(),
+    packages=find_packages('src'),
     package_dir={'':'src'},
     install_requires=[
         'numpy==1.24.3',

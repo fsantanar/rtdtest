4e3d14475fa61001c89488c0344ac55b1008f301
Felipe Santana <fsantanar@gmail.com>
2023-06-19 12:44:54 -0400

    using both src name and package_dir in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='rtdtest',
     version='1.0.0',
     packages=find_packages('src'),
+    package_dir={'':'src'},
     install_requires=[
         'numpy==1.24.3',
         'PyYAML==6.0',
